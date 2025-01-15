718b3557006679eb6d74d4b70711bad26ea0347f
zpc <zpc1131@gamil.com>
2025-01-15 22:39:56 +0800

    卸载node-sass，安装sass

--- src/App.vue
+++ src/App.vue
@@ -13,12 +13,7 @@
     <div id="tags">
       <ul v-for="item in datas" :key="item.key">
         <li>
-          <a
-            href="javascript:void(0);"
-            :style="{
-              color: '#fff',
-            }"
-          >
+          <a href="javascript:void(0);" :style="{ color: '#fff' }">
             {{ item.name ? item.name : item.key }}
             <img v-if="item.photo" :src="item.photo" :width="50" :height="50" />
           </a>
@@ -38,26 +33,26 @@
             @click="showRes = false"
             :class="{
               numberOver:
-                !!photos.find((d) => d.id === item) ||
-                !!list.find((d) => d.key === item),
+                !!photos.find(d => d.id === item) ||
+                !!list.find(d => d.key === item)
             }"
           >
-            <span class="cont" v-if="!photos.find((d) => d.id === item)">
+            <span class="cont" v-if="!photos.find(d => d.id === item)">
               <span
-                v-if="!!list.find((d) => d.key === item)"
+                v-if="!!list.find(d => d.key === item)"
                 :style="{
-                  fontSize: '40px',
+                  fontSize: '40px'
                 }"
               >
-                {{ list.find((d) => d.key === item).name }}
+                {{ list.find(d => d.key === item).name }}
               </span>
               <span v-else>
                 {{ item }}
               </span>
             </span>
             <img
-              v-if="photos.find((d) => d.id === item)"
-              :src="photos.find((d) => d.id === item).value"
+              v-if="photos.find(d => d.id === item)"
+              :src="photos.find(d => d.id === item).value"
               alt="photo"
               :width="160"
               :height="160"
@@ -92,9 +87,7 @@
     />
     <Result :visible.sync="showResult"></Result>
 
-    <span class="copy-right">
-      Copyright©zhangyongfeng5350@gmail.com
-    </span>
+    <!-- <span class="copy-right"> Copyright©zhangyongfeng5350@gmail.com </span> -->
 
     <audio
       id="audiobg"
@@ -122,7 +115,7 @@ import {
   resultField,
   newLotteryField,
   conversionCategoryName,
-  listField,
+  listField
 } from '@/helper/index';
 import { luckydrawHandler } from '@/helper/algorithm';
 import Result from '@/components/Result';
@@ -148,7 +141,7 @@ export default {
     config: {
       get() {
         return this.$store.state.config;
-      },
+      }
     },
     result: {
       get() {
@@ -156,7 +149,7 @@ export default {
       },
       set(val) {
         this.$store.commit('setResult', val);
-      },
+      }
     },
     list() {
       return this.$store.state.list;
@@ -176,13 +169,13 @@ export default {
       const nums = number >= 1500 ? 500 : this.config.number;
       const configNum = number > 1500 ? Math.floor(number / 3) : number;
       const randomShowNums = luckydrawHandler(configNum, [], nums);
-      const randomShowDatas = randomShowNums.map((item) => {
-        const listData = this.list.find((d) => d.key === item);
-        const photo = this.photos.find((d) => d.id === item);
+      const randomShowDatas = randomShowNums.map(item => {
+        const listData = this.list.find(d => d.key === item);
+        const photo = this.photos.find(d => d.id === item);
         return {
           key: item * (number > 1500 ? 3 : 1),
           name: listData ? listData.name : '',
-          photo: photo ? photo.value : '',
+          photo: photo ? photo.value : ''
         };
       });
       return randomShowDatas;
@@ -192,7 +185,7 @@ export default {
     },
     photos() {
       return this.$store.state.photos;
-    },
+    }
   },
   created() {
     const data = getData(configField);
@@ -207,7 +200,7 @@ export default {
     const newLottery = getData(newLotteryField);
     if (newLottery) {
       const config = this.config;
-      newLottery.forEach((item) => {
+      newLottery.forEach(item => {
         this.$store.commit('setNewLottery', item);
         if (!config[item.key]) {
           this.$set(config, item.key, 0);
@@ -231,7 +224,7 @@ export default {
       resArr: [],
       category: '',
       audioPlaying: false,
-      audioSrc: bgaudio,
+      audioSrc: bgaudio
     };
   },
   watch: {
@@ -241,8 +234,8 @@ export default {
         this.$nextTick(() => {
           this.reloadTagCanvas();
         });
-      },
-    },
+      }
+    }
   },
   mounted() {
     this.startTagCanvas();
@@ -282,7 +275,7 @@ export default {
       });
     },
     getPhoto() {
-      database.getAll(DB_STORE_NAME).then((res) => {
+      database.getAll(DB_STORE_NAME).then(res => {
         if (res && res.length > 0) {
           this.$store.commit('setPhotos', res);
         }
@@ -307,7 +300,7 @@ export default {
         dragControl: 1,
         textHeight: 20,
         noSelect: true,
-        lock: 'xy',
+        lock: 'xy'
       });
     },
     reloadTagCanvas() {
@@ -360,14 +353,14 @@ export default {
         }
         const oldRes = this.result[category] || [];
         const data = Object.assign({}, this.result, {
-          [category]: oldRes.concat(resArr),
+          [category]: oldRes.concat(resArr)
         });
         this.result = data;
         window.TagCanvas.SetSpeed('rootcanvas', [5, 1]);
         this.running = !this.running;
       }
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="scss">
